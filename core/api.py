1a26a8468d700a4cd34105bc0d89976863c51d3c
barish <ngosinh737@gmail.com>
2025-03-09 10:47:48 +0700

    .

--- core/api.py
+++ core/api.py
@@ -17,7 +17,7 @@ from loader import config, headers_manager as HeadersManager
 
 class APIClient:
     def __init__(self, base_url: str, account: Account):
-        self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0"
+        self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
         self.base_url = base_url
         self.account_data = account
         self.session = self._create_session()
